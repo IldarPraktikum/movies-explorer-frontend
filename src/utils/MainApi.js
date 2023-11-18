cf09e3c3393f2849dcf4eedd9b6e3e3173eb28ef
Ildar Kildiyarov <svoi-urist@yandex.ru>
2023-11-18 05:40:36 +0500

    dop

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,6 +1,6 @@
-export const BASE_URL = "https://api.ildarpracticum-dip23.nomoredomainsrocks.ru";
+export const BASE_URL = "https://api.ildarpracticum-dip23.nomoredomainsrocks.ru"; //заменить на доменное имя бэка
 
-export const handleResponse = async (response) => {
+export const handleError = async (response) => {
   if (!response.ok) {
     const res = await response.json();
     return Promise.reject(res);
@@ -15,131 +15,113 @@ export const register = (name, email, password) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ name, email, password }),
-  }).then(handleResponse);
+  }).then((res) => {
+    return handleError(res);
+  });
 };
 
-export const authorized = async (password, email) => {
-  try {
-    const response = await fetch(`${BASE_URL}/signin`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ password, email }),
+export const authorized = (password, email) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ password, email }),
+  })
+    .then((res) => {
+      return handleError(res);
+    })
+    .then((data) => {
+      if (data.token) {
+        localStorage.setItem("jwt", data.token);
+        return data;
+      }
     });
-
-    const data = await handleResponse(response);
-
-    if (data.token) {
-      localStorage.setItem("jwt", data.token);
-      return data;
-    }
-  } catch (error) {
-  }
 };
 
-export const verifyToken = async (token) => {
-  try {
-    const response = await fetch(`${BASE_URL}/users/me`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    return handleResponse(response);
-  } catch (error) {
-  }
+export const verifyToken = (token) => {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((res) => {
+    return handleError(res);
+  });
 };
 
-export const modifyUserProfile = async (data) => {
-  try {
-    const response = await fetch(`${BASE_URL}/users/me`, {
-      method: "PATCH",
-      headers: {
-        authorization: `Bearer ${localStorage.jwt}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: data.name,
-        email: data.email,
-      }),
-    });
-
-    return handleResponse(response);
-  } catch (error) {
-  }
+export const modifyUserProfile = (data) => {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "PATCH",
+    headers: {
+      authorization: `Bearer ${localStorage.jwt}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: data.name,
+      email: data.email,
+    }),
+  }).then((res) => {
+    return handleError(res);
+  });
 };
 
-export const getUserInfo = async () => {
-  try {
-    const response = await fetch(`${BASE_URL}/users/me`, {
-      headers: {
-        authorization: `Bearer ${localStorage.jwt}`,
-      },
-    });
-
-    return handleResponse(response);
-  } catch (error) {
-  }
+export const getUserInfo = () => {
+  return fetch(`${BASE_URL}/users/me`, {
+    headers: {
+      authorization: `Bearer ${localStorage.jwt}`,
+    },
+  }).then((res) => {
+    return handleError(res);
+  });
 };
 
-export const getSaveMovies = async () => {
-  try {
-    const response = await fetch(`${BASE_URL}/movies`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.jwt}`,
-      },
-    });
-
-    return handleResponse(response);
-  } catch (error) {
-  }
+export const getSaveMovies = () => {
+  return fetch(`${BASE_URL}/movies`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${localStorage.jwt}`,
+    },
+  }).then((res) => {
+    return handleError(res);
+  });
 };
 
-export const addSavedMovies = async (data) => {
-  try {
-    const response = await fetch(`${BASE_URL}/movies`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.jwt}`,
-      },
-      body: JSON.stringify({
-        country: data.country,
-        director: data.director,
-        duration: data.duration,
-        year: data.year,
-        description: data.description,
-        image: `https://api.nomoreparties.co${data.image.url}`,
-        trailerLink: data.trailerLink,
-        thumbnail: `https://api.nomoreparties.co${data.image.formats.thumbnail.url}`,
-        movieId: data.id,
-        nameRU: data.nameRU,
-        nameEN: data.nameEN,
-      }),
-    });
-
-    return handleResponse(response);
-  } catch (error) {
-  }
+export const addSavedMovies = (data) => {
+  return fetch(`${BASE_URL}/movies`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${localStorage.jwt}`,
+    },
+    body: JSON.stringify({
+      country:data.country,
+      director:data.director,
+      duration:data.duration,
+      year:data.year,
+      description:data.description,
+      image:`https://api.nomoreparties.co${data.image.url}`,
+      trailerLink:data.trailerLink,
+      thumbnail:`https://api.nomoreparties.co${data.image.formats.thumbnail.url}`,
+      movieId: data.id,
+      nameRU:data.nameRU,
+      nameEN:data.nameEN,
+    }),
+  }).then((res) => {
+    return handleError(res);
+  });
 };
 
-export const deleteSavedMovies = async (id) => {
-  try {
-    const response = await fetch(`${BASE_URL}/movies/${id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.jwt}`,
-      },
-    });
-
-    return handleResponse(response);
-  } catch (error) {
-  }
+export const deleteSavedMovies = (id) => {
+  return fetch(`${BASE_URL}/movies/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${localStorage.jwt}`,
+    },
+  }).then((res) => {
+    return handleError(res);
+  });
 };
-
